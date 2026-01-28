ee6679fc7de6b4212a385c1bc99c2503ed74e52a
mahinul0611 <mahinulislam05454@gmail.com>
2026-01-29 00:56:39 +0600

    User Signup Implemented

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
-import express, { Application, Request, Response } from "express"
+import { toNodeHandler } from "better-auth/node";
+import express, { Application, Request, Response } from "express";
+import { auth } from "./lib/auth";
+
+import cors from "cors"
 
 
-const app:Application= express();
+const app: Application = express();
+
+app.use(express.json());
+
+app.use(cors(
+    {
+    origin: process.env.APP_URL || "http://localhost:3000", //Client Side ba frontendt URL 
+    credentials:true  // Betterauth cookies alada kore set kore rakhtese tai true kore diyechi
+}
+))
 
 
-app.get("/",(req:Request,res:Response)=>{
-    
-    res.send("Hello from Server")
-})
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Server");
+});
 
 export default app;

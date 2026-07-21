574268ff5bee7157a55af7d06c097dac9259d09b
mahinul0611 <mahinulislam05454@gmail.com>
2026-07-21 15:04:51 +0600

    feat: add production frontend origin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,7 +22,7 @@ const transporter = nodemailer.createTransport({
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: [process.env.APP_URL, "http://localhost:3000" ],
   account: {
 	accountLinking: {
 		enabled: true,

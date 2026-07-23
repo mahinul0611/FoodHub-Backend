5ae3d07e1d3d12123505910b5b03399112d26ab8
mahinul0611 <mahinulislam05454@gmail.com>
2026-07-23 20:28:18 +0600

    Provider Profile Update Fi

--- src/modules/provider/provider.service.ts
+++ src/modules/provider/provider.service.ts
@@ -64,6 +64,8 @@ const updateOrderStatus = async(orderId:string,status: OrdersStatus)=>{
 
 
 const updateProfile = async (userId: string, payload: any) => {
+
+  console.log("🔥 Frontend theke asha Payload:", payload);
   const isUserExist = await prisma.user.findUnique({
     where: { id: userId },
   });
